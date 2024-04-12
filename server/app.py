c5a2dbc61d8eec04eead43d6c41e88e1238c7030
bryokn <bryokn@gmail.com>
2024-04-12 23:54:32 +0300

    Done and tests passed

--- server/app.py
+++ server/app.py
@@ -22,13 +22,21 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = Article.query.all()
+    return jsonify(articles.to_dict())
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-
-    pass
+    article = Article.query.get(id)
+    if article:
+        session['page_views'] = session.get('page_views', 0)
+        session['page_views'] += 1
+        if session['page_views'] <= 3:
+            return jsonify(article.to_dict())
+        else:
+            return jsonify({'message': 'Maximum pageview limit reached'}), 401
+    else:
+        return jsonify({'error': 'Article not found'}), 404
 
 if __name__ == '__main__':
     app.run(port=5555)
